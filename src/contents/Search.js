b54bf5e56d3283ec5417439d78d0993b9b35efb5
Madhu <madhucharliehash@gmail.com>
2021-04-04 14:49:38 +0530

    completed SEARCH component

--- src/contents/Search.js
+++ src/contents/Search.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
+import { Input } from '@material-ui/core';
+import React, { Component, useState } from 'react';
 import Navbar from '../components/Navbar';
+import Searchc from '../Styles/Searchc.css'
 
 
-class Search extends Component {
-    render() {
-        return (
+function Search() {
+    const [panNumber, setpanNumber] = useState(0);
+    const [result, setresult] = useState(false)
+    return (
             <div>
                 <Navbar />
-                <div className="condiv">
-                    <h1>Search</h1>
-                </div>
+                {result ? (
+                        <div className="condiv">
+                            {panNumber}
+                        </div>
+                ) : (
+                    <div>
+                        <div className="condiv">
+                            <div className="search">
+                            <input className="c-checkbox" type="checkbox" id="checkbox"/>
+                                <div className="c-formContainer">
+                                <form className="c-form" action="">
+                                    <input className="c-form__input" type="number" pattern="[A-Za-z]{10}" onChange={event=>setpanNumber(event.target.value)} required/>
+                                    <label className="c-form__buttonLabel" for="checkbox">
+                                    <button className="c-form__button" type="button" onClick={()=>{setresult(true)}}>Search</button>
+                                    </label>
+                                    <label className="c-form__toggle" for="checkbox"  data-title="Search"></label>
+                                </form>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
             )
-        }
-    }
-    
+}
+
 export default Search
-    
